118647f55783ea5fbee3bb062c041f7e2ec62d60
nedosika <nedosika.pp.ua>
2023-05-08 13:51:57 +0300

    remove filtering

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,48 +1,32 @@
-import React, {createContext, useContext, useMemo, useState} from 'react';
-import DataGrid, {SelectColumn, useFocusRef} from 'react-data-grid';
+import React, { useCallback, useMemo, useState} from 'react';
+import DataGrid, {SelectColumn} from 'react-data-grid';
 import 'react-data-grid/lib/styles.css';
 import {Progress} from "antd";
-import {createGlobalStyle, css, styled} from "styled-components";
 
-import classes from "./Dashboard.module.css"
-
-const FilterContext = createContext(undefined);
-
-const GlobalStyle = createGlobalStyle`
-  .filterCell {
-    line-height: 35px;
-    padding: 0;
-
-    > div {
-      padding-block: 0;
-      padding-inline: 8px;
-
-      &:first-child {
-        border-block-end: 1px solid var(--rdg-border-color);
-      }
-    }
-  }
-`;
-
-const filterContainerClassName = css`.filterCell {
-    line-height: 35px;
-    padding: 0;
-
-    > div {
-      padding-block: 0;
-      padding-inline: 8px;
-
-      &:first-child {
-        border-block-end: 1px solid var(--rdg-border-color);
-      }
-    }
-  }`;
-
-const filterClassName = css`
-  inline-size: 100%;
-  padding: 4px;
-  font-size: 14px;
-`;
+const columns = [
+    SelectColumn,
+    {
+        name: 'Date',
+        key: 'date',
+    },
+    {
+        name: 'Name',
+        key: 'name',
+    },
+    {
+        name: 'Status',
+        key: 'status',
+    },
+    {
+        name: "Progress",
+        key: 'progress',
+        formatter: ({row: {progress}}) => <Progress percent={progress} size="small"/>
+    },
+    {
+        name: 'End Date',
+        key: 'end_date',
+    },
+];
 
 const rows = [
     {
@@ -71,102 +55,40 @@ const rows = [
     },
 ];
 
-function inputStopPropagation(event) {
-    if (['ArrowLeft', 'ArrowRight'].includes(event.key)) {
-        event.stopPropagation();
-    }
-}
-function FilterRenderer({
-    isCellSelected,
-    column,
-    children
-}) {
-    const filters = useContext(FilterContext);
-    const { ref, tabIndex } = useFocusRef(isCellSelected);
-
-    return (
-        <>
-            <div>{column.name}</div>
-            {filters.enabled && <div>{children({ ref, tabIndex, filters })}</div>}
-        </>
-    );
-}
-
 const Dashboard = () => {
     const [selectedRows, setSelectedRows] = useState(() => new Set());
-    const [filters, setFilters] = useState({
-        name: '',
-        enabled: false
-    });
+    const [sortColumns, setSortColumns] = useState([]);
 
-    const columns = useMemo(() => [
-        SelectColumn,
-        {
-            name: 'Date',
-            key: 'date',
-        },
-        {
-            name: 'Name',
-            key: 'name',
-            headerCellClass: filters.enabled ? 'filterCell' : undefined,
-            headerRenderer: (p) => {
-                return <FilterRenderer {...p}>
-                    {({filters, ...rest}) => {
-                        return (
-                            <input
-                                {...rest}
-                                className={filterClassName}
-                                value={filters.name}
-                                onChange={(e) =>
-                                    setFilters({
-                                        ...filters,
-                                        name: e.target.value
-                                    })
-                                }
-                                onKeyDown={inputStopPropagation}
-                                list='names'
-                            />
-                        );
-                    }}
-                </FilterRenderer>;
-            }
-        },
-        {
-            name: 'Status',
-            key: 'status',
-        },
-        {
-            name: "Progress",
-            key: 'progress',
-            formatter: ({row: {progress}}) => <Progress percent={progress} size="small"/>
-        },
-        {
-            name: 'End Date',
-            key: 'end_date',
-        },
-    ], [filters]);
+    const onSortColumnsChange = useCallback((sortColumns) => {
+        setSortColumns(sortColumns.slice(-1));
+    }, []);
 
-    const filteredRows = useMemo(() => {
-        return rows.filter((r) => {
-            return (
-                (filters.name ? r.name.includes(filters.name) : true)
-            );
-        });
-    }, [filters]);
+    const sortedRows = useMemo(() => {
+        if (sortColumns.length === 0) return rows;
+        const { columnKey, direction } = sortColumns[0];
+
+        let sortedRows = [...rows];
+
+        switch (columnKey) {
+            case 'name':
+                sortedRows = sortedRows.sort((a, b) => a[columnKey].localeCompare(b[columnKey]));
+                break;
+            default:
+        }
+        return direction === 'DESC' ? sortedRows.reverse() : sortedRows;
+    }, [sortColumns]);
 
     return (
-        <>
-            <GlobalStyle/>
-            <FilterContext.Provider value={filters}>
                 <DataGrid
                     columns={columns}
-                    rows={filteredRows}
+                    rows={sortedRows}
                     selectedRows={selectedRows}
                     onSelectedRowsChange={setSelectedRows}
+                    sortColumns={sortColumns}
+                    onSortColumnsChange={onSortColumnsChange}
                     defaultColumnOptions={{
                         sortable: true,
-                        resizable: true,
-                        filterable: true
+                        resizable: true
                     }}
                     onCellClick={(args, event) => {
                         if (args.column.key === 'name') {
@@ -175,10 +97,7 @@ const Dashboard = () => {
                         }
                     }}
                     rowKeyGetter={(row) => row.id}
-                    headerRowHeight={filters.enabled ? 70 : undefined}
                 />
-            </FilterContext.Provider>
-        </>
     );
 }
 
